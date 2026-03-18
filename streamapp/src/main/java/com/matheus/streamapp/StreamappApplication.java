df1f9e5ad62a58ad031dfa8e12b56ffdbc4b7680
Matheus-Correa-Fiori <math_fiori@hotmail.com>
2026-03-18 13:24:49 -0300

    chore: initial Spring Boot project setup

---
+++ streamapp/src/main/java/com/matheus/streamapp/StreamappApplication.java
@@ -0,0 +1,13 @@
+package com.matheus.streamapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamappApplication.class, args);
+	}
+
+}
---
+++ streamapp/src/test/java/com/matheus/streamapp/StreamappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheus.streamapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
